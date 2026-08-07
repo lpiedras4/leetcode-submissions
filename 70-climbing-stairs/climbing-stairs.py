d22b79daacc13767a99f38940f7586b2c9184e19
Leonardo Piedras <piedras.leo@gmail.com>
2026-08-06 17:29:50 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.46%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -10,3 +10,26 @@ class Solution:
         for i in range(3, n + 1):
             ways[i] = ways[i - 1] + ways[i  - 2]
         return ways[n]
+"""
+3 stairs
+Option 1:
+1 step + 1 step + 1 step
+Option 2:
+1 step + 2 steps
+Option 3:
+2 steps + 1 step
+
+Subproblem
+ways(i) = number of distinct ways to reach i.
+Base cases
+ways(1) = 1 (a normal step)
+ways(2) = 2 (a jump)
+
+Formula that connects
+ways(i) = ways(i - 1) = ways(i -2) 
+
+
+Bottom-up approach
+start from base cases and build a table forward
+
+""" 
