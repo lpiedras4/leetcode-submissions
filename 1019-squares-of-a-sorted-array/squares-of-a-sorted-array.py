b12c359e1e310d6a60bb4c00191afe4e4df852ba
Leonardo Piedras <piedras.leo@gmail.com>
2026-08-23 16:01:57 -0700

    Time: 12 ms (38.19%) | Memory: 20.5 MB (95.90%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,5 +1,7 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        #O(n log n) -> Time
+        # O(1) -> Space
         for i in range(len(nums)):
             num = nums[i]
             square_num = abs(num) * abs(num)
